d16ddcdf26a2cf5f00e85d172424ac248bdc09d9
Ajdin Ljubunčić <ajdin.ljubuncic@edu.fit.ba>
2024-06-30 21:19:52 +0200

    displayed other book data and created nice responsive design for inital page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    screens: {
+      'mobile': '320px',
+      'tablet': '640px',
+      'desktop': '1024px',
+    },
+    extend: {
+      colors: {
+        customOrange: 'rgb(214, 106, 67)',
+      },
+      spacing: {
+        '120': '120%',
+        '900': '900px',
+      },
+      width: {
+        'calc-33p-40': 'calc(33.33% - 40px)',
+        'calc-50p-40': 'calc(50% - 40px)'
+      },
+    },
   },
   plugins: [],
 }
